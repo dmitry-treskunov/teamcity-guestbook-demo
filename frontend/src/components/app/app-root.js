c9336e3363fffdcec106b0f0eb302dd8ae512645
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2019-04-30 11:29:29 -0700

    someone was here

--- frontend/src/components/app/app-root.js
+++ frontend/src/components/app/app-root.js
@@ -11,7 +11,7 @@ export default () => (
       <a href="/">
         <Logo glyph={jbLogo} size={Logo.Size.Size48} />
       </a>
-      <h2>{'Sam was here'}</h2>
+      <h2>{'Someone was here'}</h2>
     </Header>
     <div className="app-content">
       <Guestbook />
