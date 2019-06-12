5648aba98d36323c124bedcaa99dbeab238052e4
Dmitry Treskunov <dmitry-treskunov@users.noreply.github.com>
2019-06-12 04:26:37 -0700

    Drop all tests

--- backend/src/test/java/guestbook/GuestBookEntryTest.java
+++ backend/src/test/java/guestbook/GuestBookEntryTest.java
@@ -7,17 +7,5 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GuestBookEntryTest {
 
-    @Test
-    void test_author() {
-        GuestBookEntry entry = new GuestBookEntry("author", "Hello!", "imageUrl", now());
-
-        assertEquals("author", entry.getAuthor());
-    }
-
-    @Test
-    void test_message() {
-        GuestBookEntry entry = new GuestBookEntry("author", "Hello!", "imageUrl", now());
-
-        assertEquals("Hello!", entry.getMessage());
-    }
+ 
 }
